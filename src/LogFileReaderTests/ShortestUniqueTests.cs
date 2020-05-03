6e995c25c6355aa1d7d7a998450d433421ab5a8d
Ronny Hanssen <ronny@intellisearch.com>
2020-05-03 17:38:41 +0200

    Commit irrelevant for the LogExpress application

--- src/LogFileReaderTests/ShortestUniqueTests.cs
+++ src/LogFileReaderTests/ShortestUniqueTests.cs
@@ -28,126 +28,126 @@ namespace Flexy.Tests
                 .CreateLogger();
         }
 
-        [Test]
-        public void Find_ShortestUnique_Start_And_Length_Differ_Test()
-        {
-            var input = new[]
-            {
-                "path/to/a/file.txt",
-                "another/path/to/a/file.txt"
-            };
-            var expected = new[]
-            {
-                "      …a/file.txt", 
-                "…er/pa…a/file.txt"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_Start_And_Length_Differ_Test()
+{
+    var input = new[]
+    {
+        "path/to/a/file.txt",
+        "another/path/to/a/file.txt"
+    };
+    var expected = new[]
+    {
+        "      …a/file.txt", 
+        "…er/pa…a/file.txt"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_Start_Differ_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/d/src/path/to/a/file.txt"
-            };
-            var expected = new[]
-            {
-                "…c…a/file.txt", 
-                "…d…a/file.txt"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_Start_Differ_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/d/src/path/to/a/file.txt"
+    };
+    var expected = new[]
+    {
+        "…c…a/file.txt", 
+        "…d…a/file.txt"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_Start_Differ_Without_Skip_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/d/src/path/to/a/file.txt"
-            };
-            var expected = new[]
-            {
-                "…c…a/file.txt", 
-                "…d…a/file.txt"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_Start_Differ_Without_Skip_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/d/src/path/to/a/file.txt"
+    };
+    var expected = new[]
+    {
+        "…c…a/file.txt", 
+        "…d…a/file.txt"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_End_Differ_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/c/src/path/to/a/file.dat"
-            };
-            var expected = new[]
-            {
-                "…a/file.txt", 
-                "…a/file.dat"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_End_Differ_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/c/src/path/to/a/file.dat"
+    };
+    var expected = new[]
+    {
+        "…a/file.txt", 
+        "…a/file.dat"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_Middle_Differ_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/c/src/path/to/b/file.txt"
-            };
-            var expected = new[]
-            {
-                "…a/file.txt", 
-                "…b/file.txt"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_Middle_Differ_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/c/src/path/to/b/file.txt"
+    };
+    var expected = new[]
+    {
+        "…a/file.txt", 
+        "…b/file.txt"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_Start_And_End_Differ_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/d/src/path/to/a/file.dat"
-            };
+[Test]
+public void Find_ShortestUnique_Start_And_End_Differ_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/d/src/path/to/a/file.dat"
+    };
 
-            var expected = new[]
-            {
-                "…a/file.txt", 
-                "…a/file.dat"  
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+    var expected = new[]
+    {
+        "…a/file.txt", 
+        "…a/file.dat"  
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        [Test]
-        public void Find_ShortestUnique_Start_Middle_And_End_Differ_Test()
-        {
-            var input = new[]
-            {
-                "/c/src/path/to/a/file.txt",
-                "/d/src/path/to/b/file.dat"
-            };
-            var expected = new[]
-            {
-                "…a/file.txt",
-                "…b/file.dat"
-            };
-            AssertShortestUniqueResults(input, expected);
-        }
+[Test]
+public void Find_ShortestUnique_Start_Middle_And_End_Differ_Test()
+{
+    var input = new[]
+    {
+        "/c/src/path/to/a/file.txt",
+        "/d/src/path/to/b/file.dat"
+    };
+    var expected = new[]
+    {
+        "…a/file.txt",
+        "…b/file.dat"
+    };
+    AssertShortestUniqueResults(input, expected);
+}
 
-        private static void AssertShortestUniqueResults(IReadOnlyList<string> input, IReadOnlyList<string> expected)
-        {
-            var (actual, success) = AbbreviatedUniquePath(input);
-            Assert.That(success, Is.EqualTo(true));
-            Assert.That(actual.Count, Is.EqualTo(expected.Count));
-            for (var i = 0; i < expected.Count; i++) Assert.That(actual[i], Is.EqualTo(expected[i]));
-        }
+private static void AssertShortestUniqueResults(IReadOnlyList<string> input, IReadOnlyList<string> expected)
+{
+    var (actual, success) = AbbreviatedUniquePath(input);
+    Assert.That(success, Is.EqualTo(true));
+    Assert.That(actual.Count, Is.EqualTo(expected.Count));
+    for (var i = 0; i < expected.Count; i++) Assert.That(actual[i], Is.EqualTo(expected[i]));
+}
 
     }
 }
